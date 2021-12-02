29099f46d4c8b16a0dda336167d5d35e3cdbd258
Bart Skowron <bart.skowron@rollbar.com>
2021-12-02 15:28:42 +0100

    Format code to PSF standards

--- fetch_people_for_items.py
+++ fetch_people_for_items.py
@@ -24,8 +24,8 @@ import requests  # version 2
 
 def fetch_item_id_for_counter(access_token, item_counter):
     resp = requests.get(
-        'https://api.rollbar.com/api/1/item_by_counter/%d' % item_counter,
-        headers={'X-Rollbar-Access-Token': access_token},
+        "https://api.rollbar.com/api/1/item_by_counter/%d" % item_counter,
+        headers={"X-Rollbar-Access-Token": access_token},
         allow_redirects=False,
     )
 
@@ -33,7 +33,7 @@ def fetch_item_id_for_counter(access_token, item_counter):
         print(resp)
         raise Exception("Got an API error while fetching an item by counter")
 
-    return resp.json()['result']['itemId']
+    return resp.json()["result"]["itemId"]
 
 
 def fetch_people_for_item_id(access_token, output_file, item_id):
@@ -46,9 +46,9 @@ def fetch_people_for_item_id(access_token, output_file, item_id):
             break
 
         for instance in page_data:
-            person_data = instance['data'].get('person')
+            person_data = instance["data"].get("person")
             if person_data:
-                output_file.write(json.dumps(person_data) + '\n')
+                output_file.write(json.dumps(person_data) + "\n")
                 output_file.flush()
 
         page_number += 1
@@ -56,27 +56,27 @@ def fetch_people_for_item_id(access_token, output_file, item_id):
 
 def fetch_page(access_token, item_id, page=1):
     resp = requests.get(
-        'https://api.rollbar.com/api/1/item/%d/instances/?page=%d' % (item_id, page),
-        headers={'X-Rollbar-Access-Token': access_token},
+        "https://api.rollbar.com/api/1/item/%d/instances/?page=%d" % (item_id, page),
+        headers={"X-Rollbar-Access-Token": access_token},
     )
 
     if resp.status_code != 200:
         print(resp)
         raise Exception("Got an error from the Rollbar API")
-    
-    return resp.json()['result']['instances']
 
-    
-if __name__ == '__main__':
+    return resp.json()["result"]["instances"]
+
+
+if __name__ == "__main__":
     if len(sys.argv) < 4:
         print("Usage: python fetch_people_for_items.py <read access token> <output filename> <item id 1> [<item id 2> ...]")
         sys.exit(1)
-    
+
     access_token = sys.argv[1]
     output_filename = sys.argv[2]
     item_counters = [int(x) for x in sys.argv[3:]]
 
-    output_file = open(output_filename, 'w')
+    output_file = open(output_filename, "w")
 
     for item_counter in item_counters:
         item_id = fetch_item_id_for_counter(access_token, item_counter)
